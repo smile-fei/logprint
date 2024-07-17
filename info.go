71eb44d05969f3949b05206c6a0307aba283dae8
smile-fei <96099243+smile-fei@users.noreply.github.com>
2024-07-17 16:38:39 +0800

    Update info.go

--- info.go
+++ info.go
@@ -9,6 +9,6 @@ import (
 
 func Info(msg interface{}) {
 	t := time.Now()
-	fmt.Printf("[info] %s:%s \n",t.Format("2006-01-02 15:04:05"),msg)
+	fmt.Printf("[info]-[info] %s:%s \n",t.Format("2006-01-02 15:04:05"),msg)
 }
 
